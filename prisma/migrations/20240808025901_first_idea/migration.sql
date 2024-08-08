29b8ae87749801b4fb96810faa515930fc856bf9
Anghelo Alva <ance.anghelo@gmail.com>
2024-08-08 11:56:39 -0400

    chore: Add Prisma configuration files and Docker Compose file

---
+++ prisma/migrations/20240808025901_first_idea/migration.sql
@@ -0,0 +1,159 @@
+-- CreateTable
+CREATE TABLE "User" (
+    "id" TEXT NOT NULL,
+    "username" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Board" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "description" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Board_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "List" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "boardId" TEXT NOT NULL,
+
+    CONSTRAINT "List_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Card" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "description" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "dueDate" TIMESTAMP(3) NOT NULL,
+    "listId" TEXT NOT NULL,
+
+    CONSTRAINT "Card_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Comment" (
+    "id" TEXT NOT NULL,
+    "text" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "userId" TEXT NOT NULL,
+    "cardId" TEXT NOT NULL,
+
+    CONSTRAINT "Comment_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Label" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "color" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "boardId" TEXT NOT NULL,
+
+    CONSTRAINT "Label_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Role" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "boardId" TEXT NOT NULL,
+
+    CONSTRAINT "Role_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "_BoardToUser" (
+    "A" TEXT NOT NULL,
+    "B" TEXT NOT NULL
+);
+
+-- CreateTable
+CREATE TABLE "_CardToLabel" (
+    "A" TEXT NOT NULL,
+    "B" TEXT NOT NULL
+);
+
+-- CreateTable
+CREATE TABLE "_RoleToUser" (
+    "A" TEXT NOT NULL,
+    "B" TEXT NOT NULL
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_username_key" ON "User"("username");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "_BoardToUser_AB_unique" ON "_BoardToUser"("A", "B");
+
+-- CreateIndex
+CREATE INDEX "_BoardToUser_B_index" ON "_BoardToUser"("B");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "_CardToLabel_AB_unique" ON "_CardToLabel"("A", "B");
+
+-- CreateIndex
+CREATE INDEX "_CardToLabel_B_index" ON "_CardToLabel"("B");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "_RoleToUser_AB_unique" ON "_RoleToUser"("A", "B");
+
+-- CreateIndex
+CREATE INDEX "_RoleToUser_B_index" ON "_RoleToUser"("B");
+
+-- AddForeignKey
+ALTER TABLE "List" ADD CONSTRAINT "List_boardId_fkey" FOREIGN KEY ("boardId") REFERENCES "Board"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Card" ADD CONSTRAINT "Card_listId_fkey" FOREIGN KEY ("listId") REFERENCES "List"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Comment" ADD CONSTRAINT "Comment_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Comment" ADD CONSTRAINT "Comment_cardId_fkey" FOREIGN KEY ("cardId") REFERENCES "Card"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Label" ADD CONSTRAINT "Label_boardId_fkey" FOREIGN KEY ("boardId") REFERENCES "Board"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Role" ADD CONSTRAINT "Role_boardId_fkey" FOREIGN KEY ("boardId") REFERENCES "Board"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "_BoardToUser" ADD CONSTRAINT "_BoardToUser_A_fkey" FOREIGN KEY ("A") REFERENCES "Board"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "_BoardToUser" ADD CONSTRAINT "_BoardToUser_B_fkey" FOREIGN KEY ("B") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "_CardToLabel" ADD CONSTRAINT "_CardToLabel_A_fkey" FOREIGN KEY ("A") REFERENCES "Card"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "_CardToLabel" ADD CONSTRAINT "_CardToLabel_B_fkey" FOREIGN KEY ("B") REFERENCES "Label"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "_RoleToUser" ADD CONSTRAINT "_RoleToUser_A_fkey" FOREIGN KEY ("A") REFERENCES "Role"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "_RoleToUser" ADD CONSTRAINT "_RoleToUser_B_fkey" FOREIGN KEY ("B") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
---
+++ prisma/seed/seed.ts
@@ -0,0 +1,217 @@
+import { prisma } from "../../src/domain/prisma"
+
+async function main() {
+	console.log("Start seeding ...")
+
+	await prisma.comment.deleteMany()
+	await prisma.label.deleteMany()
+	await prisma.card.deleteMany()
+	await prisma.list.deleteMany()
+	await prisma.board.deleteMany()
+	await prisma.role.deleteMany()
+	await prisma.user.deleteMany()
+
+	const user1 = await prisma.user.create({
+		data: {
+			email: "user1@gmail.com",
+			password: "password",
+			username: "user1",
+		},
+	})
+
+	const user2 = await prisma.user.create({
+		data: {
+			email: "user2@gmail.com",
+			password: "password",
+			username: "user2",
+		},
+	})
+
+	const board1 = await prisma.board.create({
+		data: {
+			name: "Board 1",
+			description: "Board 1 description",
+			users: {
+				connect: {
+					id: user1.id,
+				},
+			},
+		},
+	})
+
+	const board2 = await prisma.board.create({
+		data: {
+			name: "Board 2",
+			description: "Board 2 description",
+			users: {
+				connect: {
+					id: user2.id,
+				},
+			},
+		},
+	})
+
+	const list1 = await prisma.list.create({
+		data: {
+			name: "List 1",
+			boardId: board1.id,
+		},
+	})
+
+	const list2 = await prisma.list.create({
+		data: {
+			name: "List 2",
+			boardId: board1.id,
+		},
+	})
+
+	const list3 = await prisma.list.create({
+		data: {
+			name: "List 3",
+			boardId: board2.id,
+		},
+	})
+
+	const card1 = await prisma.card.create({
+		data: {
+			name: "Card 1",
+			description: "Card 1 description",
+			dueDate: new Date('2024-09-01T00:00:00.000Z'),
+			listId: list1.id,
+		},
+	})
+
+	const card2 = await prisma.card.create({
+		data: {
+			name: "Card 2",
+			description: "Card 2 description",
+			dueDate: new Date('2024-09-01T00:00:00.000Z'),
+			listId: list1.id,
+		},
+	})
+
+	const card3 = await prisma.card.create({
+		data: {
+			name: "Card 3",
+			description: "Card 3 description",
+			dueDate: new Date('2024-09-01T00:00:00.000Z'),
+			listId: list2.id,
+		},
+	})
+
+	const card4 = await prisma.card.create({
+		data: {
+			name: "Card 4",
+			description: "Card 4 description",
+			dueDate: new Date('2024-09-01T00:00:00.000Z'),
+			listId: list3.id,
+		},
+	})
+
+	const label1 = await prisma.label.create({
+		data: {
+			name: "Label 1",
+			color: "red",
+			boardId: board1.id,
+			cards: {
+				connect: {
+					id: card1.id,
+				},
+			}
+		},
+	})
+
+	const label2 = await prisma.label.create({
+		data: {
+			name: "Label 2",
+			color: "blue",
+			boardId: board1.id,
+			cards: {
+				connect: {
+					id: card2.id,
+				},
+			},
+		},
+	})
+
+	const label3 = await prisma.label.create({
+		data: {
+			name: "Label 3",
+			color: "green",
+			boardId: board1.id,
+			cards: {
+				connect: {
+					id: card3.id,
+				},
+			},
+		},
+	})
+
+	const role1 = await prisma.role.create({
+		data: {
+			name: "Admin",
+			users: {
+				connect: {
+					id: user1.id,
+				},
+			},
+			boardId: board1.id,
+		},
+	})
+
+	const role2 = await prisma.role.create({
+		data: {
+			name: "Member",
+			users: {
+				connect: {
+					id: user2.id,
+				},
+			},
+			boardId: board1.id,
+		},
+	})
+
+	const role3 = await prisma.role.create({
+		data: {
+			name: "Member",
+			users: {
+				connect: {
+					id: user1.id,
+				},
+			},
+			boardId: board2.id,
+		},
+	})
+
+	const comment1 = await prisma.comment.create({
+		data: {
+			text: "Comment 1",
+			cardId: card1.id,
+			userId: user1.id,
+		},
+	})
+
+	const comment2 = await prisma.comment.create({
+		data: {
+			text: "Comment 2",
+			cardId: card1.id,
+			userId: user2.id,
+		},
+	})
+
+	const comment3 = await prisma.comment.create({
+		data: {
+			text: "Comment 3",
+			cardId: card2.id,
+			userId: user1.id,
+		},
+	})
+
+	console.log("Seeding complete.")
+}
+
+;(() => {
+	if (process.env.NODE_ENV === "production") return
+
+	main()
+})()
